62effa6b542ab6437717331080a43af94c05d0d8
Adriel Alves <98328671+adriel45dev@users.noreply.github.com>
2023-03-26 19:00:19 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,32 @@
 const planters = [
-  { name: "Alice", treesPlanted: 5000 },
-  { name: "Bob", treesPlanted: 4500 },
-  { name: "Carol", treesPlanted: 3500 },
-  { name: "David", treesPlanted: 2000 },
-];
+        { name: "Alice", treesPlanted: 5000 },
+        { name: "Bob", treesPlanted: 4500 },
+        { name: "Carol", treesPlanted: 3500 },
+        { name: "David", treesPlanted: 2000 },
+      ];
+
+      function displayLeaderboard() {
+        const leaderboardList = document.getElementById("leaderboardList");
+        leaderboardList.innerHTML = "";
+
+        planters.sort((a, b) => b.treesPlanted - a.treesPlanted);
+
+        planters.forEach((planter, index) => {
+          const item = `<li class="border border-gray-200 rounded-md p-4 flex items-center justify-between">
+                <div class="flex items-center">
+                  <span class="text-2xl font-bold text-green-600 mr-4">${
+                    index + 1
+                  }</span>
+                  <div>
+                    <p class="text-xl">${planter.name}</p>
+                    <p class="text-gray-500">${planter.treesPlanted} pontos</p>
+                  </div>
+                </div>
+                <span class="text-2xl font-bold">${planter.treesPlanted}</span>
+              </li>`;
+
+          leaderboardList.innerHTML += item;
+        });
+      }
+
+      displayLeaderboard();
