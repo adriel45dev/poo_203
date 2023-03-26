d92897b03c02c3460875e8832d4117ed9bf5cbdc
Adriel Alves <98328671+adriel45dev@users.noreply.github.com>
2023-03-26 18:58:50 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,6 @@
+const planters = [
+  { name: "Alice", treesPlanted: 5000 },
+  { name: "Bob", treesPlanted: 4500 },
+  { name: "Carol", treesPlanted: 3500 },
+  { name: "David", treesPlanted: 2000 },
+];
